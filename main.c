26bf8a819a0997fc072e6df64827003f0a1d737c
Anil Nell <anil@xordev.io>
2025-11-18 23:13:23 -0500

    added bias

--- main.c
+++ main.c
@@ -17,20 +17,46 @@ float rand_float(void)
   return (float) rand()/(float) RAND_MAX;
 }
 
+// x1, x2, x3, ... b
+// w1, w2, w3, ...
+// y = x1*w1 + x2*w2 + x3*w3 + ... + b
+float cost(float w, float b)
+{
+    float result = 0.0f;
+    for(size_t i =0; i < train_count; ++i) {
+        float x = train[i][0];
+        float y = x*w + b;
+        float d = y - train[i][1];
+        result += d*d;
+    }
+    result /= train_count;
+    return result;
+}
 int main()
 {
     //srand(time(0));
     srand(69);
     //y = X*W;
     float w = rand_float()*10.0f;
+    float b = rand_float()*5.0f;
+    printf("w: %f\n", w);
+    printf("b: %f\n", b);
+    
+    float eps = 1e-3;
+    float rate = 1e-3;
 
-    for(size_t i =0; i < train_count; ++i) {
-        float x = train[i][0];
-        float y = x*w;
-        printf("actual: %f, expexted %f\n", y, train[i][0]);
+    printf("cost= %f\n", cost(w, b));
+    for(size_t i = 0; i < 10000; ++i) {
+        float c = cost(w, b);
+        float dw = (cost(w + eps, b) - c)/eps;
+        float db = (cost(w, b + eps) - c)/eps;
+        w -= rate*dw;
+        b -= rate*db;
+        printf("cost = %f, w = %f, b = %f\n", c, w, b);
     }
 
-    printf("%f\n", w);
-    printf("Hello world\n");
+    printf("----------------------------\n");
+    printf("w = %f, b = %f\n", w, b);
+    
     return 0;
 }
