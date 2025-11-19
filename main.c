f2b555ab5277874cbebddcd6820b54258c5dd6fe
Anil Nell <anil@xordev.io>
2025-11-18 22:03:36 -0500

    initial commit

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+float train[][2] = {
+    {0, 0},
+    {1, 2},
+    {2, 4},
+    {3, 6},
+    {4, 8}
+};
+
+int main()
+{
+    //y = X*W;
+    printf("Hello world\n");
+    return 0;
+}
